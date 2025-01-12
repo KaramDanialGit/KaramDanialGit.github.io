5062122dfa057c3f01c9097f87562225159414d5
KaramDanialGit <karamdanial31@gmail.com>
2025-01-12 17:58:21 -0500

    UGHHHH:

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4005504375"
+    serviceWorkerVersion: "3721946873"
   }
 });
